9b21b9e88910ae75a824deff5ac8c0aca2f2402b
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-12-02 00:31:30 +0100

    working on day1, solved part 1

--- day1/main_day1.cpp
+++ day1/main_day1.cpp
@@ -2,7 +2,46 @@
 // Created by Dino on 01.12.2023.
 //
 #include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
 
 int main() {
+  vector<int> calibration_values{};
+
+  string line{};
+  ifstream i_stream{"input.txt"};
+  if (i_stream.is_open()) {
+    while (getline(i_stream, line)) {
+      char f_num{0};
+      char l_num{0};
+      bool found_first{false};
+
+      for (const char ch: line) {
+        if (isdigit(ch)) {
+          if (!found_first) {
+            f_num = ch;
+            found_first = true;
+          }
+
+          l_num = ch;
+        }
+      }
+      string pair{f_num};
+      pair += l_num;
+      const int pair_int = stoi(pair);
+      calibration_values.push_back(pair_int);
+    }
+    int sum{0};
+    for (const int cv : calibration_values) {
+      sum += cv;
+    }
+    cout << sum;
+  }
+  else {
+    cout << "failed to open file";
+  }
+  i_stream.close();
   return 0;
 }
