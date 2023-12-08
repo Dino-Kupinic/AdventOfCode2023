efc319f858736f378b6b7071366b2ee5e608fb2b
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-12-09 00:55:31 +0100

    working on day2

--- day2/main_day2.cpp
+++ day2/main_day2.cpp
@@ -17,6 +17,16 @@ struct Game {
   vector<Set> sets;
 };
 
+vector<string> split_line_into_segments(const string&line) {
+  vector<string> temp{};
+  istringstream stream{line};
+  string segment{};
+  while (getline(stream, segment, ';')) {
+    temp.push_back(segment);
+  }
+  return temp;
+}
+
 int main() {
   vector<Game> games{};
   pair<int, string> max_red{12, "red"};
@@ -26,17 +36,23 @@ int main() {
   string line{};
   ifstream i_stream{"example.txt"};
   if (i_stream.is_open()) {
-    regex game_id_regex{"(\\d+)"};
+    regex game_id_regex{R"(\d+)"};
+    regex cube_regex{R"([\d]+\s[\w]+(?:,[\d]+\s[\w]+)*)"};
 
     while (getline(i_stream, line)) {
       Game temp_game{};
-      sregex_iterator id_iterator(line.begin(), line.end(), game_id_regex);
-      sregex_iterator end;
-      // match_results::begin?
-      for (sregex_iterator i = id_iterator; i != end; ++i) {
-        smatch match = *i;
-        std::string match_str = match.str();
-        std::cout << "  " << match_str << '\n';
+      if (smatch matcher; regex_search(line, matcher, game_id_regex)) {
+        const int game_id = stoi(matcher[0]);
+
+        size_t idx_of_colon = line.find(':');
+        vector<string> games_played = split_line_into_segments(line.substr(idx_of_colon));
+        for (const auto& game: games_played) {
+          for (auto i = sregex_iterator(game.begin(), game.end(), cube_regex); i != sregex_iterator(); ++i) {
+            const smatch&matches = *i;
+            std::cout << matches.str() << '\n';
+          }
+          std::cout<<"End of game"<<std::endl;
+        }
       }
     }
   }
