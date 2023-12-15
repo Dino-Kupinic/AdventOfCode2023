103bdd9346f9964084f19c923072e72991c6af14
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-12-15 18:53:43 +0100

    working on day2

--- day2/main_day2.cpp
+++ day2/main_day2.cpp
@@ -27,32 +27,38 @@ vector<string> split_line_into_segments(const string&line) {
   return temp;
 }
 
-int main() {
-  vector<Game> games{};
-  pair<int, string> max_red{12, "red"};
-  pair<int, string> max_green{13, "green"};
-  pair<int, string> max_blue{14, "blue"};
-
+void parse_games(vector<Game>&games) {
   string line{};
   ifstream i_stream{"example.txt"};
+
   if (i_stream.is_open()) {
-    regex game_id_regex{R"(\d+)"};
-    regex cube_regex{R"([\d]+\s[\w]+(?:,[\d]+\s[\w]+)*)"};
+    const regex game_id_regex{R"(\d+)"};
+    const regex cube_regex{R"([\d]+\s[\w]+(?:,[\d]+\s[\w]+)*)"};
 
     while (getline(i_stream, line)) {
       Game temp_game{};
       if (smatch matcher; regex_search(line, matcher, game_id_regex)) {
-        const int game_id = stoi(matcher[0]);
+        Set temp_set;
 
-        size_t idx_of_colon = line.find(':');
-        vector<string> games_played = split_line_into_segments(line.substr(idx_of_colon));
-        for (const auto& game: games_played) {
-          for (auto i = sregex_iterator(game.begin(), game.end(), cube_regex); i != sregex_iterator(); ++i) {
+        const int game_id = stoi(matcher[0]);
+        const size_t idx_colon = line.find(':');
+        const vector<string> sets_played = split_line_into_segments(line.substr(idx_colon));
+
+        for (const auto&set: sets_played) {
+          for (auto i = sregex_iterator(set.begin(), set.end(), cube_regex); i != sregex_iterator(); ++i) {
             const smatch&matches = *i;
-            std::cout << matches.str() << '\n';
+            const string cube_string = matches.str();
+            const size_t idx_whitespace = cube_string.find(' ');
+            const int cube_amount = stoi(cube_string.substr(0, idx_whitespace));
+            const string cube_color = cube_string.substr(idx_whitespace + 1);
+            const pair temp_pair{cube_amount, cube_string};
+            temp_set.cubes.push_back(temp_pair);
+            cout << cube_amount << cube_color << "\n";
           }
-          std::cout<<"End of game"<<std::endl;
+          temp_game.id = game_id;
+          temp_game.sets.push_back(temp_set);
         }
+        games.push_back(temp_game);
       }
     }
   }
@@ -60,5 +66,24 @@ int main() {
     cout << "failed to open file";
   }
   i_stream.close();
+}
+
+int main() {
+  vector<Game> games{};
+  pair<int, string> max_red{12, "red"};
+  pair<int, string> max_green{13, "green"};
+  pair<int, string> max_blue{14, "blue"};
+
+  parse_games(games);
+  for (const auto&[id, sets]: games) {
+    int sum_red{};
+    int sum_green{};
+    int sum_blue{};
+    for (const auto&[cubes]: sets) {
+      for (const auto&round: cubes) {
+
+      }
+    }
+  }
   return 0;
 }
